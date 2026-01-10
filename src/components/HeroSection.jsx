de464a4365432d0a59873760413c61d097f4ef54
Eltaf Sameem <140640813+Eltaf-Sameem@users.noreply.github.com>
2026-01-10 18:40:04 -0500

    added Hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,29 @@
+export const HeroSection = () => {
+    return (
+        <section
+            id="hero"
+            className="relative min-h-screen flex flex-col items-center justify-center px-4"
+        >
+            <div className="container max-w-4xl mx-auto text-center z-10">
+                <div className="space-y-6">
+                    
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                        <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+                        <span className="text-primary opacity-0 animate-fade-in-delay-1"> Eltaf</span>
+                        <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Sameem</span>
+                    </h1>
+
+                    <p className="text-lg md:text-xl text-muted-foreground max-w-4xl mx-auto opacity-0 animate-fade-in-delay-3"> {/*was max-2-2xl*/ }
+                        I engineer scalable back-end systems using modern technologies.
+                        Specializing in cloud infrastructure, APIs, and AI-driven services,
+                        I design solutions that are secure, reliable, and built to perform under real-world demands
+                    </p>
+
+     
+
+                </div>
+            </div>
+
+
+        </section>);
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { StarBackground } from "@/components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { NavBar } from "../components/Navbar";
+import { HeroSection } from "@/components/HeroSection";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -15,6 +16,9 @@ export const Home = () => {
         <NavBar />
 
         {/* Main Content */}
+        <main>
+            <HeroSection />
+        </main>
 
         {/* Footer */}
 
